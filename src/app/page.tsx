db853c62ad2568a3dcbdf6cd866f5692ae7a9848
its-rayan <rayanmabdirahman@gmail.com>
2025-11-02 00:02:19 +0000

    added logo and favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <main>
+      <main className="bg-black">
+        <Image
+          className="dark:invert"
+          src="/logo.svg"
+          alt="Rein logomark"
+          width={64}
+          height={64}
+        />
         <h1>hello world</h1>
         <Button>Click me</Button>
       </main>
