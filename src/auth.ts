49f665770e814ed66e125cbf3dec09a19da66ac7
its-rayan <rayanmabdirahman@gmail.com>
2025-11-02 17:00:37 +0000

    config next-auth to use mongo db

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,17 @@
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
+import connectMongoAdaptor from "./database/connect-mongo-adaptor";
+
+const mongoClient = await connectMongoAdaptor();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(mongoClient),
+
+  // Override default NextAuth pages
   pages: {
     signIn: "/auth/signin",
     error: "/auth/signin"
   },
+
   providers: []
 });
