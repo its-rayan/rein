8ded645123402762beddfed6ef32bf747e66a079
its-rayan <rayanmabdirahman@gmail.com>
2025-11-03 00:32:05 +0000

    added nextauth callbacks to add additional user details for email sign in

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,7 @@
 import authEdgeConfig from "@/auth-edge.config";
+import connectToDbClient from "@/database/connect-db-client";
 import connectMongoAdaptor from "@/database/connect-mongo-adaptor";
+import User from "@/database/models/user";
 import sendEmail from "@/lib/emails";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import NextAuth from "next-auth";
@@ -48,5 +50,72 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET
     })
-  ]
+  ],
+
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      // Custom sign in logic to add additional user details to magic link users
+      console.log("SignIn callback:", {
+        user,
+        account,
+        profile,
+        email,
+        credentials
+      });
+
+      // check if the user is signing in with email provider
+      if (account?.type === "email") {
+        // Add additional user details to the user object
+        const username = user?.email?.split("@")[0];
+        const updatedUserObj = {
+          ...user,
+          name: username,
+          image: `https://api.dicebear.com/6.x/initials/svg?seed=${username}`
+        };
+
+        console.log("Updated user object:", updatedUserObj);
+
+        await connectToDbClient();
+
+        const userExists = await User.findOne({ email: user?.email });
+        if (!userExists) {
+          await User.create(updatedUserObj);
+        } else {
+          await User.updateOne({ email: user?.email }, updatedUserObj);
+        }
+      }
+
+      return true;
+    },
+
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+        session.user.email = token.email as string;
+        session.user.image = token.picture as string;
+      }
+
+      return session;
+    },
+
+    async jwt({ token, user }) {
+      await connectToDbClient();
+      const dbUser = await User.findOne({ email: token.email });
+
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id;
+        }
+        return token;
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image
+      };
+    }
+  }
 });
