d78b787e386dd3216ed164799a90d33ac3df8592
its-rayan <rayanmabdirahman@gmail.com>
2025-11-06 11:57:39 +0000

    add progress bar boilerplate

--- src/app/dashboard/tasks/page.tsx
+++ src/app/dashboard/tasks/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
 import { cn } from "@/lib/utils";
@@ -5,6 +6,71 @@ import { getFormattedCurrentDate } from "@/utils/datetime/get-current-date";
 import { Check } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
+
+const ProgressBar = ({ todos, length }: any) => {
+  const percentage = Math.floor((todos.length / length) * 100);
+
+  return (
+    <div className="flex flex-col items-end gap-1">
+      <div className="flex gap-1">
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 10
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 20
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 30
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 40
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 50
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 60
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 70
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 80
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 90
+          })}
+        ></div>
+        <div
+          className={cn("h-5 w-5 rounded-sm bg-neutral-300", {
+            "bg-green-600": percentage >= 100
+          })}
+        ></div>
+      </div>
+
+      <div className="text-muted-foreground text-sm">
+        <span>{`${todos.length} / ${length} Tasks Completed`}</span>
+      </div>
+    </div>
+  );
+};
 
 export default function TasksPage() {
   const [todos, setTodos] = useState([
@@ -40,6 +106,8 @@ export default function TasksPage() {
     }
   ]);
 
+  const completedTodo = todos.filter((todo) => todo.completed);
+
   const toggleTask = (id: number) => {
     const updatedTodos = todos.map((todo) =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
@@ -49,7 +117,7 @@ export default function TasksPage() {
 
   return (
     <div className="mx-auto max-w-7xl">
-      <div className="flex flex-col gap-6">
+      <div className="mx-auto flex w-3xl flex-col gap-6">
         {/* header */}
         <div className="flex w-full items-start justify-between">
           <div className="flex flex-col">
@@ -59,7 +127,7 @@ export default function TasksPage() {
             <h1 className="text-xl font-medium">Today&apos;s Todos</h1>
           </div>
 
-          <h1>Progress bar</h1>
+          <ProgressBar todos={completedTodo} length={todos.length} />
         </div>
 
         {/* Todos */}
