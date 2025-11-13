7d97c37f033865bdaa42316704cf1ac5bdb6b54c
its-rayan <rayanmabdirahman@gmail.com>
2025-11-13 23:28:57 +0000

    update user db model to include goals array

--- src/database/models/goal.ts
+++ src/database/models/goal.ts
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
 const goalSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  title: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true, trim: true },
   description: { type: String },
-  deadline: { type: Date },
+  deadline: { type: Date, required: true },
   status: {
     type: String,
     enum: ["not_started", "in_progress", "completed", "abandoned"],
     default: "not_started"
   },
-  progress: { type: Number }
+  progress: { type: Number, default: 0 }
 });
 
 const Goal = mongoose.models.Goal || mongoose.model("Goal", goalSchema);
--- src/database/models/user.ts
+++ src/database/models/user.ts
@@ -2,9 +2,16 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, trim: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
   image: { type: String },
-  emailVerified: { type: Date }
+  emailVerified: { type: Date },
+  goals: [{ type: mongoose.Schema.Types.ObjectId, ref: "Goal" }]
 });
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
