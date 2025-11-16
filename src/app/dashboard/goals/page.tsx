6f55bc66d951c1606df171934f9d79e0c6502a05
its-rayan <rayanmabdirahman@gmail.com>
2025-11-16 16:58:44 +0000

    remove prefetching of data from the goals page as it is not needed

--- src/app/dashboard/goals/page.tsx
+++ src/app/dashboard/goals/page.tsx
@@ -1,32 +1,21 @@
 import CreateGoalDialog from "@/components/goals/create-goal-dialog";
 import GoalList from "@/components/goals/goal-list";
-import { getGoals } from "@/data/goal/goal.loader";
-import { getQueryClient } from "@/lib/react-query/get-query-client";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-export default async function GoalsPage() {
-  const queryClient = getQueryClient();
-  await queryClient.prefetchQuery({
-    queryKey: ["goals"],
-    queryFn: getGoals
-  });
-
+export default function GoalsPage() {
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <div className="flex flex-col gap-10">
-        <div className="flex justify-between">
-          <div className="max-w-md">
-            <h1 className="text text-lg font-semibold">Goals 🎯</h1>
-            <p className="text-muted-foreground">
-              You do not rise to the level of your goals, you fall to the level
-              of your systems.
-            </p>
-          </div>
-          <CreateGoalDialog />
+    <div className="flex flex-col gap-10">
+      <div className="flex justify-between">
+        <div className="max-w-md">
+          <h1 className="text text-lg font-semibold">Goals 🎯</h1>
+          <p className="text-muted-foreground">
+            You do not rise to the level of your goals, you fall to the level of
+            your systems.
+          </p>
         </div>
-
-        <GoalList />
+        <CreateGoalDialog />
       </div>
-    </HydrationBoundary>
+
+      <GoalList />
+    </div>
   );
 }
--- src/components/goals/goal-list.tsx
+++ src/components/goals/goal-list.tsx
@@ -1,22 +1,44 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { getGoals } from "@/data/goal/goal.loader";
 import { useQuery } from "@tanstack/react-query";
 
+function GoalListSkeleton() {
+  return (
+    <div className="flex flex-col gap-4">
+      {Array.from(Array(5).keys()).map((key) => (
+        <Skeleton key={key} className="h-[125px] w-full rounded-xl" />
+      ))}
+    </div>
+  );
+}
+
+function EmptyGoalList() {
+  return (
+    <div>
+      <h1 className="text-4xl">No goals yet!</h1>
+    </div>
+  );
+}
+
 export default function GoalList() {
-  const { data: goals } = useQuery({
+  const { data: goals, isLoading } = useQuery({
     queryKey: ["goals"],
     queryFn: getGoals
   });
 
-  if (!goals) {
-    return <p>No loagaes</p>;
+  if (isLoading) {
+    return <GoalListSkeleton />;
   }
 
-  console.log("getGoals: ", goals);
+  if (!goals?.length) {
+    return <EmptyGoalList />;
+  }
+
   return (
     <div>
-      {goals.map((goal) => (
+      {goals?.map((goal) => (
         <li key={goal.id}> {goal.name}</li>
       ))}
     </div>
