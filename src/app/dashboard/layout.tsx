36f27c52605ea250f12208374b89fa769c46edda
its-rayan <rayanmabdirahman@gmail.com>
2025-12-01 21:54:55 +0000

    add sidebar layout to dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,15 @@
-import MainNav from "@/components/layout/main-nav";
-import AuthProvider from "@/components/providers/auth-provider";
-
 export default function DashboardLayout({
   children
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="mx-auto min-h-screen w-full">
-      <AuthProvider>
-        <MainNav />
-        <main className="mx-auto max-w-7xl pt-4">{children}</main>
-      </AuthProvider>
-    </div>
+    // <div className="mx-auto min-h-screen w-full">
+    //   <AuthProvider>
+    //     <MainNav />
+    //     <main className="mx-auto max-w-7xl pt-4">{children}</main>
+    //   </AuthProvider>
+    // </div>
+    <div>{children}</div>
   );
 }
