b6744c1454e30b621cf0fdf9985747ecefddfad3
its-rayan <rayanmabdirahman@gmail.com>
2025-12-06 14:09:57 +0000

    add more dropdown and delete functionality

--- src/components/goals/goal-list.tsx
+++ src/components/goals/goal-list.tsx
@@ -1,9 +1,94 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
 import { Skeleton } from "@/components/ui/skeleton";
-import { getGoals } from "@/data/goal/goal.loader";
-import { useQuery } from "@tanstack/react-query";
+import { deleteGoal, getGoals } from "@/data/goal/goal.loader";
+import { getQueryClient } from "@/lib/react-query/get-query-client";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  ArrowUpRight,
+  Link as LinkIcon,
+  MoreHorizontal,
+  Trash2
+} from "lucide-react";
 import Link from "next/link";
+
+function MoreDropdown({ goalId }: { goalId?: string }) {
+  const queryClient = getQueryClient();
+  const mutation = useMutation({
+    mutationFn: (goalId: string) => {
+      return deleteGoal(goalId);
+    },
+    onMutate: async (goalId: string) => {
+      // Cancel any outgoing refetches
+      // (so they don't overwrite our optimistic update)
+      await queryClient.cancelQueries({ queryKey: ["goals"] });
+
+      // Snapshot the previous value
+      const previousGoals = queryClient.getQueryData(["goals"]);
+
+      // Optimistically update to the new value after deletion
+      queryClient.setQueryData(["goals"], (oldGoals: any) => {
+        if (!oldGoals) return oldGoals;
+        return oldGoals.filter((goal: any) => goal.id !== goalId);
+      });
+
+      // Return a context object with the snapshotted value
+      return { previousGoals };
+    },
+    onError: (err, goalId, context: any) => {
+      queryClient.setQueryData(["goals"], context.previousGoals);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["goals"] });
+    }
+  });
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="size-6 self-end rounded-full bg-white/20 p-0 hover:bg-white/50 hover:text-white"
+        >
+          <MoreHorizontal />
+          <span className="sr-only">More</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        className="w-56 rounded-lg"
+        side="right"
+        align="start"
+      >
+        <DropdownMenuItem>
+          <LinkIcon className="text-muted-foreground" />
+          <span>Copy Link</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <ArrowUpRight className="text-muted-foreground" />
+          <span>Open in New Tab</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          onClick={() => {
+            mutation.mutate(goalId as string);
+          }}
+        >
+          <Trash2 className="text-muted-foreground" />
+          <span>Delete</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
 
 function GoalListSkeleton() {
   return (
@@ -44,6 +129,7 @@ export default function GoalList() {
           key={goal.id}
           className="relative flex h-52 flex-col rounded-lg bg-amber-500 p-4 text-white"
         >
+          <MoreDropdown goalId={goal.id} />
           <Link
             href={`/dashboard/goals/${goal.id}?name=${goal.name}&description=${goal.description}`}
             className="flex h-full w-full flex-col"
